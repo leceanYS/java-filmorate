22381cc84060c26aa4c91f88e270688d2692b053
leceanYS <antibiotic-2@yandex.ru>
2023-10-13 21:32:55 +0300

    controllers-films-users

---
+++ src/main/java/ru/yandex/practicum/controllers/UserController.java
@@ -0,0 +1,72 @@
+package ru.yandex.practicum.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.exceptions.ValidationException;
+import ru.yandex.practicum.model.User;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+    private int id;
+    private final Map<Integer, User> users = new HashMap<>();
+
+    // получение списка всех пользователей
+    @GetMapping
+    public List<User> getAllUsers() {
+        log.info("Список всех пользователей {} отправлен клиенту", users.values());
+
+        return new ArrayList<>(users.values());
+    }
+
+    // создание пользователя
+    @PostMapping
+    public User postUser(@Valid @RequestBody User user) {
+        if (user.getId() != 0) {
+            log.warn("В метод POST передан id пользователя");
+            throw new ValidationException("В метод POST нельзя передавать id пользователя");
+        }
+
+        checkName(user);
+        user.setId(++id);
+
+        users.put(user.getId(), user);
+        log.info("Пользователь добавлен {}", user);
+
+        return user;
+    }
+
+    // обновление пользователя
+    @PutMapping
+    public User putUser(@Valid @RequestBody User user) {
+        boolean isThereAnId = users.containsKey(user.getId());
+        if (!isThereAnId) {
+            log.warn("В метод PUT передан пользователь с несуществующим id");
+            throw new ValidationException("Пользователя с таким id нет");
+        }
+
+        checkName(user);
+
+        // Если все проверки пройдены, то user обновляется.
+        users.put(user.getId(), user);
+        log.info("Пользователь обновлен {}", user);
+
+        return user;
+    }
+
+    // Если имя пустое, то login становится именем.
+    private void checkName(User user) {
+        boolean isNameCorrect = (user.getName() != null) && (!user.getName().isEmpty());
+
+        if (!isNameCorrect) {
+            user.setName(user.getLogin());
+        }
+    }
+}
---
+++ src/test/java/ru/yandex/practicum/filmorate/controllers/UserControllerTest.java
@@ -0,0 +1,83 @@
+package ru.yandex.practicum.filmorate.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.model.User;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.time.LocalDate;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+    private Validator validator;
+    private Set<ConstraintViolation<User>> violations;
+    private User user;
+
+    @BeforeEach
+    public void create() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+
+        user = new User(0,
+                "name",
+                "login",
+                LocalDate.now(),
+                "user@gmail.com");
+    }
+
+    @Test
+    public void userWithCorrectParametersShouldBeValidated() {
+        violations = validator.validate(user);
+
+        assertTrue(violations.isEmpty());
+    }
+
+    // Проверка на граничные с валидацией значения
+    @Test
+    public void userMustNotBeValidatedWithAnEmptyEmailOrNotContainingSignAt() {
+        user.setEmail(null);
+        violations = validator.validate(user);
+
+        assertEquals(1, violations.size());
+
+        user.setEmail("usergmail.com");
+        violations = validator.validate(user);
+
+        assertEquals(1, violations.size());
+    }
+
+    @Test
+    public void userMustNotValidateWithAnEmptyOrContainingSpacesLogin() {
+        user.setLogin("");
+        violations = validator.validate(user);
+
+        // Тут 2, потому что отрабатывает сразу 2 аннотации.
+        assertEquals(2, violations.size());
+
+        user.setLogin("login name");
+        violations = validator.validate(user);
+
+        assertEquals(1, violations.size());
+    }
+
+    @Test
+    public void userMustBeValidatedIfUsernameIsEmpty() {
+        user.setName("");
+        violations = validator.validate(user);
+
+        assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    public void userShouldNotBeValidatedIfHisDateOfBirthIsInTheFuture() {
+        user.setBirthday(LocalDate.now().plusDays(1));
+        violations = validator.validate(user);
+
+        assertEquals(1, violations.size());
+    }
+}
