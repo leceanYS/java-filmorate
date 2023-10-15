aa8896776a2e545a45e91f8d32c0204065171744
leceanYS <antibiotic-2@yandex.ru>
2023-10-15 20:33:43 +0300

    controllers-films-users

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -7,7 +7,6 @@ import ru.yandex.practicum.filmorate.validannotation.FilmReleaseDateConstraint;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Positive;
-import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
