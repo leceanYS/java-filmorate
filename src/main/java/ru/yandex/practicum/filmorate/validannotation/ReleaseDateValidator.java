319cc8fdea395f957af6ff5b36cd61eba1e7338b
leceanYS <antibiotic-2@yandex.ru>
2023-10-17 21:38:55 +0300

    controllers-films-users

--- src/main/java/ru/yandex/practicum/filmorate/validannotation/ReleaseDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validannotation/ReleaseDateValidator.java
@@ -10,6 +10,6 @@ public class ReleaseDateValidator implements ConstraintValidator<FilmReleaseDate
     @Override
     public boolean isValid(LocalDate releaseDate, ConstraintValidatorContext constraintValidatorContext) {
         return releaseDate.equals(LocalDate.of(1895, 12, 28)) ||
-                releaseDate.isAfter(LocalDate.of(1895, 12, 28)) ;
+                releaseDate.isAfter(LocalDate.of(1895, 12, 28));
     }
 }
