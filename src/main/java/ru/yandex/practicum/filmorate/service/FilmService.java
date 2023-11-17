d27fb07d07a54048e2dcfb7d333aef6cc268f4f5
leceanYS <antibiotic-2@yandex.ru>
2023-11-17 22:14:51 +0300

    controllers-films-users

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
 import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
-import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
 
 import java.util.Comparator;
 import java.util.List;
