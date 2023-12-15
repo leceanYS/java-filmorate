f83ab849d533c3f8b71a3046c09fc947a68a1a97
leceanYS <135338781+leceanYS@users.noreply.github.com>
2023-12-15 07:51:43 +0300

    controllers-films-users  1st2

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/AlreadyExistException.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.practicum.filmorate.exceptions;
-
-public class AlreadyExistException extends RuntimeException {
-
-    public AlreadyExistException(String message) {
-        super(message);
-    }
-}
