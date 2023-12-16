22fdeb36fbc230580cd490103e5e6f02c0eafc07
leceanYS <135338781+leceanYS@users.noreply.github.com>
2023-12-16 23:30:33 +0300

    add-database 1st2

--- src/test/java/ru/yandex/practicum/filmorate/controllers/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controllers/FilmControllerTest.java
@@ -11,7 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
--import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import ru.yandex.practicum.filmorate.model.Film;
